68216839619e1d36bbcf120ed5b605d64b8fb026
anisort19 <valeria.gorpinich.58@gmail.com>
2026-02-15 22:43:47 +0200

    add branch for lab1 (#1)
    Co-authored-by: anisort <jaki.anisort@gmail.com>

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-
+    // lab1
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader =
