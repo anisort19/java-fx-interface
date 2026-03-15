79eacea3d7f5410e3e4d8305d724541847d8f866
anisort <jaki.anisort@gmail.com>
2026-03-16 00:05:53 +0200

    add lab7

--- src/main/java/com/example/demo/squares/Controller.java
+++ src/main/java/com/example/demo/squares/Controller.java
@@ -1,7 +1,10 @@
 package com.example.demo.squares;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
@@ -23,6 +26,9 @@ public class Controller {
 
     @FXML
     private Rectangle br;
+
+    @FXML
+    private Rectangle c1, c2, c3, c4, c5, c6, c7;
 
     @FXML
     public void initialize() {
@@ -60,5 +66,47 @@ public class Controller {
 
         br.xProperty().bind(root.widthProperty().subtract(br.widthProperty()));
         br.yProperty().bind(root.heightProperty().subtract(br.heightProperty()));
+
+        // палітра
+        setupColor(c1);
+        setupColor(c2);
+        setupColor(c3);
+        setupColor(c4);
+        setupColor(c5);
+        setupColor(c6);
+        setupColor(c7);
+    }
+
+    private void setupColor(Rectangle rect){
+
+        rect.setOnMouseClicked(e -> {
+
+            Color baseColor = (Color) rect.getFill();
+
+            if(e.getButton() == MouseButton.PRIMARY){
+                center.setFill(baseColor);
+            }
+
+            if(e.getButton() == MouseButton.SECONDARY){
+
+                double hue = baseColor.getHue();
+                double saturation = baseColor.getSaturation();
+                double brightness = baseColor.getBrightness();
+
+                saturation = Math.max(0, saturation - 0.4);
+                brightness = Math.min(1.0, brightness + 0.1);
+
+                Color lighter = Color.hsb(hue, saturation, brightness);
+
+                center.setFill(lighter);
+            }
+
+        });
+
+    }
+
+    @FXML
+    private void exitApp(){
+        Platform.exit();
     }
 }
